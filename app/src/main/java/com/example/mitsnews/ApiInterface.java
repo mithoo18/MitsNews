b365ab0ed522422854a73983047aecabb94e46ce
mithoo <devanshu1g8@gmail.com>
2020-09-08 13:03:56 +0530

    check 1

---
+++ app/src/main/java/com/example/mitsnews/ApiInterface.java
@@ -0,0 +1,19 @@
+package com.example.mitsnews;
+
+import com.example.mitsnews.Model.Headlines;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("top-headlines")
+    Call<Headlines> getHeadlines(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey);
+
+    @GET("everything")
+    Call<Headlines> getSpecificData(
+            @Query("q") String query,
+            @Query("apiKey") String apiKey);
+}
---
+++ app/src/main/java/com/example/mitsnews/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.mitsnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.mitsnews.Model.Articles;
+import com.example.mitsnews.Model.Headlines;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    Adapter adapter;
+    SwipeRefreshLayout swipeRefreshLayout;
+    Button btnSearch;
+    EditText etQuery;
+
+    final String API_KEY= "f6eeb8c0601c46f5a19cfaef0b01fd19";
+    List<Articles> articles = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //binding
+        recyclerView = findViewById(R.id.recyclerView);
+        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
+        btnSearch = findViewById(R.id.btnSearch);
+        etQuery = findViewById(R.id.etQuery);
+
+        //recycler
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        final String country = getCountry();
+
+        //when you swipe it will refresh
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                retrieveJson("", country, API_KEY);//without search query
+            }
+        });
+
+        //it just show the data
+        retrieveJson("", country, API_KEY);//bec query is empty
+
+        //when you click on search and pass query
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //query should not be null
+                    if (!etQuery.getText().toString().equals("")) {
+                        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                            @Override
+                            public void onRefresh() {
+                            //query pass
+                            retrieveJson(etQuery.getText().toString(), country, API_KEY);
+                             }
+                            });
+                        retrieveJson(etQuery.getText().toString(), country, API_KEY);
+                    }else{
+                        //when query is null
+                    swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                        @Override
+                        public void onRefresh() {
+                            retrieveJson("", country, API_KEY);
+                            //we have not send any query
+                        }
+                        });
+                    retrieveJson("", country, API_KEY);
+                }
+            }
+        });
+    }
+    public void retrieveJson(String query,String country,String apiKey){
+        swipeRefreshLayout.setRefreshing(true);
+        Call<Headlines> call;
+
+        //not null - everything
+        if(!etQuery.getText().toString().equals(""))
+        {
+            call = ApiClient.getIntance().getApi().getSpecificData(query,apiKey);
+        }else{
+            //null - top-trending
+            call = ApiClient.getIntance().getApi().getHeadlines(country,apiKey);
+        }
+
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+            if(response.isSuccessful() && response.body().getArticles()!=null)
+            {
+                swipeRefreshLayout.setRefreshing(false);
+                articles.clear();
+                articles = response.body().getArticles();
+                adapter = new Adapter(MainActivity.this,articles);
+                recyclerView.setAdapter(adapter);
+            }
+            }
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(MainActivity.this,t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public String getCountry()
+    {
+        Locale locale = Locale.getDefault();
+        String country = locale.getCountry();
+        return country.toLowerCase();
+    }
+
+}
